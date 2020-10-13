db037ae347d09cd4cdec2fd1ef7b1e7c4aaa8a79
JaCrowe <jacrowe@mta.ca>
2020-10-12 23:11:01 -0400

    Makes use of Roboto font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
         body {
           margin: 0px;
           padding: 0px;
+          font-family: "Roboto", Arial, Helvetica, sans-serif;
         }
       `}</style>
       <Component {...pageProps} />
