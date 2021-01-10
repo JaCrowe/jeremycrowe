48c98b3a0304ca153f5706311e8c848403648773
JaCrowe <jacrowe@mta.ca>
2021-01-10 12:34:21 -0400

    Removed Mastodon link, I'm not up to much there

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,7 +1,7 @@
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles } from "@material-ui/styles";
 
 export default function SocialLinks() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <div className={classes.container}>
       {socialLinks.map(({ link, src, alt }) => (
@@ -10,50 +10,50 @@ export default function SocialLinks() {
         </a>
       ))}
     </div>
-  )
+  );
 }
 
 type SocialLink = {
-  link: string
-  src: string
-  alt: string
-}
+  link: string;
+  src: string;
+  alt: string;
+};
 
 const socialLinks: SocialLink[] = [
+  // {
+  //   link: 'https://mastodon.online/@JaCrowe',
+  //   src: '/images/mastodon.svg',
+  //   alt: "Jeremy Crowe's github account",
+  // },
   {
-    link: 'https://mastodon.online/@JaCrowe',
-    src: '/images/mastodon.svg',
+    link: "https://pixelfed.social/JaCrowe",
+    src: "/images/pixelfed-icon-color.svg",
     alt: "Jeremy Crowe's github account",
   },
   {
-    link: 'https://pixelfed.social/JaCrowe',
-    src: '/images/pixelfed-icon-color.svg',
+    link: "https://github.com/JaCrowe",
+    src: "/images/github.svg",
     alt: "Jeremy Crowe's github account",
   },
   {
-    link: 'https://github.com/JaCrowe',
-    src: '/images/github.svg',
-    alt: "Jeremy Crowe's github account",
-  },
-  {
-    link: 'https://www.linkedin.com/in/jeremyacrowe/',
-    src: '/images/linkedin.svg',
+    link: "https://www.linkedin.com/in/jeremyacrowe/",
+    src: "/images/linkedin.svg",
     alt: "Jeremy Crowe's LinkedIn profile",
   },
-]
+];
 
 const useStyles = makeStyles({
   container: {
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
-    maxWidth: '500px',
-    margin: 'auto',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    maxWidth: "500px",
+    margin: "auto",
+    justifyContent: "center",
   },
   socialIcon: {
-    width: '32px',
-    height: '32px',
-    margin: '4px',
+    width: "32px",
+    height: "32px",
+    margin: "4px",
   },
-})
+});
